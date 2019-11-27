59bb4a9311b000eebc555905a898d1962d42d34f
HaeEunMyung <hayluna2@gmail.com>
2019-11-27 17:21:05 +0900

    웹소켓 구현

--- app.js
+++ app.js
@@ -4,6 +4,18 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// 웹소켓 
+var server = require('http').createServer(app); //웹 소켓을 위한 새로운 서버를 만든다.
+
+//웹소켓 서버는 3001번에서 listening하고 있다.
+server.listen(3001, function(){
+  console.log('socket io server listening on port 3001');
+})
+
+//socket연결 및 on, event행동정보가 담긴 socket.js모듈을 불러온다.
+//socket.js모듈은 웹서버를 파라미터로 받는 함수이다.
+require('./socket.js')(server); // 웹소켓 함수 실행
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var tempRouter = require('./routes/temp');
@@ -18,7 +30,15 @@ var app = express();
 // 몽고디비: express객체 생긴 후 몽고디비 연결
 connect();
 
-app.use(cors()) //cors use
+//웹소켓통신을 위해서는 쌍방 cors설정이 되어야한다.
+//클라이언트의 주소를 허용한다.
+var corsOptions = {
+  origin: 'http://localhost:8080',
+  credentials: true };
+
+app.use(cors(corsOptions));
+// app.use(cors()) //cors use
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
---
+++ socket.js
@@ -0,0 +1,25 @@
+const SocketIO = require('socket.io');
+
+//함수형 모듈
+module.exports = (server) =>{
+    const io = SocketIO(server, {path:'/socket.io'});
+
+    io.on('connection', (socket)=>{
+        const req = socket.request;
+        const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+        console.log('새로운 클라이언트 접속!', ip, socket.id, req.ip);
+        socket.on('disconnect', ()=>{
+            console.log('클라이언트 접속 해제', ip, socket.id);
+            clearInterval(socket.interval);
+        });
+        socket.on('error', (error)=>{
+            console.error(error);
+        });
+        socket.on('reply', (data)=>{ //on: reply란 이름의 통신을 대기중이다
+            console.log(data);
+        });
+        socket.interval = setInterval(()=>{
+            socket.emit('news', 'Hello Socket.IO'); //emit: news란 이름의 통신으로 보낸다. 클라이언트로 내보낼 메시지:Hello Socket.IO
+        },3000);
+    })
+}
