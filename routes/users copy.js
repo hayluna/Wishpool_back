0e7f6f1b7e890ef6298361bc517df76da7d6b76d
kjh <kim.jihye.dev@gmail.com>
2019-12-04 15:13:07 +0900

    비번 암호화 완료. 암호 일치여부 확인 완료

--- routes/users copy.js
+++ routes/users copy.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const bcrypt =require('bcrypt');
+const bcrpyt =require('bcrypt');
 // const jwt = require('jsonwebtoken');
 // const { verifyToken } = require('./middlewares'); 
 
@@ -18,14 +18,20 @@ router.get('/', (req,res,next)=>{
 });  
 
 // 회원가입
-router.post('/entry',async (req,res,next)=> {
+router.post('/entry',(req,res,next)=> {
   // 기존에 가입된 회원인지 확인(나중에 처리)
 
 
 
 // 미가입 상태일 경우 신규회원 가입 처리
 // 비밀번호 암호화 처리
-  const hash = bcrypt.hashSync(req.body.password,12);
+  const hash = bcrpyt.hash(req.body.password,12,(err,hash)=>{
+    if(err){
+      console.log('hasherr');
+    }else if(hash){
+      console.log('hash~');
+    }
+  });
 
 
   const user = new User({
@@ -33,6 +39,7 @@ router.post('/entry',async (req,res,next)=> {
     userName:req.body.userName,
     userId:req.body.userId,
     password:hash,
+    // password:req.body.password,
     email:req.body.email,
     phone:req.body.phone,
     nickName:req.body.nickName,
@@ -43,7 +50,7 @@ router.post('/entry',async (req,res,next)=> {
   user.save()
     .then(function(result){
       console.log(result);
-      res.json({ code: 200, result: '가입성공' });
+      res.status(200).json(result);
 
     })
     .catch(function (err) {
@@ -54,14 +61,9 @@ router.post('/entry',async (req,res,next)=> {
 
 // 로그인 처리
 router.post('/login',(req,res,next)=>{
-  
-  // 아이디가 존재하는지 DB에서 확인하여 User객체를 불러옴
-  // Java 때와는 달리 리턴값은 User객체이다
-  // 이 바로 아래 코드로는 Object라고만 나오지 user객체의 정보는 안 보임
-  // const exitUser = User.findOne({"userId":req.body.userId});
+  // 아이디가 존재하는지 DB에서 확인
+  const exitUser = User.findOne({"userId":req.body.userId});
 
-  // 이런식으로 불러온 후 콜백 함수를 작성해서 처리해야 한다
-  // user가 바로 찾아온 user객체. user의 정보를 볼 수가 있다
   User.findOne({userId:req.body.userId},function(err,user){
     // 에러발생 처리
       if(err){
@@ -71,17 +73,25 @@ router.post('/login',(req,res,next)=>{
     // 일치하는 사용자가 있을 경우
       if(user){
         console.log('가입된사용자');
-        console.log('user정보:'+ user);
-        // console.log('이용자정보:'+exitUser);
-        // 비밀번호가 일치하는지 확인하기
-        const comparePwd = bcrypt.compareSync(req.body.password,user.password);
-          if(comparePwd){
-            console.log('일치여부'+ comparePwd);
-            
-          }else{
-            console.log('불일치' + comparePwd);
-          }
-        
+        // 비밀번호가 일치하는지 확인
+        // 콜백함수에서 err은 error, 그 뒤는 결과에 해당하는 객체
+        User.findOne({password:req.body.password},function(err,pwd){
+            if(err){
+              console.log('비번찾다 에러발생');
+              next(err);
+            }
+          // 비번이 일치할 경우 
+            if(pwd){
+              console.log('id,pwd일치');
+
+            }else{
+            // 비번이 일치하지 않을 경우  
+              console.log('불일치');
+
+
+            }
+
+        });
         // 일치하는 사용자가 없을 경우
       }else if(!user){
         console.log('미등록 사용자');
