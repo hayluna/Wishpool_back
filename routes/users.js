cbebe3935f0d1d920dc37e924037c634861f6c04
HaeEun Myung <hayluna2@gmail.com>
2019-12-10 09:35:49 +0900

    충돌해결

--- routes/users.js
+++ routes/users.js
@@ -18,17 +18,15 @@ router.get('/', (req,res,next)=>{
 });  
 
 // 회원가입
-router.post('/entry',async (req,res,next)=> {
+// 가입하자마자 로그인 되도록 처리
+router.post('/entry',(req,res,next)=> {
   // 기존에 가입된 회원인지 확인(나중에 처리)
 
 
-
-
-
-  // 미가입 상태일 경우 신규회원 가입 처리
+  // 단방향 해쉬 암호화(해독불가)
   const hash = bcrypt.hashSync(req.body.password,12);
-
-
+  
+  // 미가입 상태일 경우 신규회원 가입 처리
   const user = new User({
 
     userName:req.body.userName,
@@ -44,8 +42,17 @@ router.post('/entry',async (req,res,next)=> {
   user.save()
     .then(function(result){
       console.log(result);
-      res.json({ code: 200, result: '가입성공' });
 
+      // 회원 가입과 동시에 로그인하게끔 처리
+      const token = jwt.sign({
+        userId:user.userId,
+      },process.env.JWT_SECRET,{
+        expiresIn: '1d',
+        issuer:'wishlist'
+      });
+
+      
+      return res.json({ code:200, result:token});
     })
     .catch(function (err) {
       console.error(err);
@@ -73,7 +80,7 @@ router.post('/temp', async (req,res, next)=>{
 // 로그인 처리
 router.post('/login',(req,res,next)=>{
   
-  User.findOne({userId:req.body.userId},function(err,user){
+  User.findOne({userId:req.body.userId,userState:true},function(err,user){
     // 에러발생 처리
       if(err){
         console.error('에러발생');
@@ -81,43 +88,59 @@ router.post('/login',(req,res,next)=>{
       }
     // 일치하는 사용자가 있을 경우
       if(user){
-
         const comparePwd = bcrypt.compareSync(req.body.password,user.password);
           if(comparePwd){
             
-            // usrId,nickName을 토큰에 담는다
             const token = jwt.sign({
               userId:user.userId,
-              nickName:user.nickName
             },process.env.JWT_SECRET,{
-              expiresIn: '100m',
+              expiresIn: '1d',
               issuer:'wishlist'
             });
 
             console.log(token);
             return res.json({ code:200, result:token});
             
-            
           }else{
             console.log('불일치' + comparePwd);
+            return res.json({code:500,message:'암호가 일치하지 않습니다'});
           }
         
         // 일치하는 사용자가 없을 경우
       }else if(!user){
         console.log('미등록 사용자');
+        return res.json({code:500,message:'등록된 사용자가 아닙니다'});
       }
-
-
     })
 });
 
 // 회원 정보 조회
+router.get('/profile',verifyToken,(req,res)=>{
+  const userId = req.decoded.userId;
 
-router.get('/register',verifyToken,(req,res)=>{
+    User.findOne({
+      userId:userId
+    },(err,user)=>{
+      if(err){
+        console.error('에러발생');
+        return res.status(500).json({code:500,message:'서버에러'});
+      }
+
+      if(user){
+        console.log(user);
+        return res.json({code:200,result:user});
+      }else{
+        return res.json({code:500,message:'유효하지 않은 회원입니다'});
+      }
+    })
+} )
+
+// 로그아웃
+router.get('/logout',verifyToken,(req,res)=>{
   console.log(req.decoded);
   const userId = req.decoded.userId;
 
-    const userData = User.findOne({
+    User.findOne({
       userId:userId
     },(err,user)=>{
       
@@ -125,15 +148,18 @@ router.get('/register',verifyToken,(req,res)=>{
         console.error('에러발생');
         return res.status(500).json({code:500,message:'서버에러'});
       }
-
-      console.log('user정보:' + userData);
-      console.log('real user정보:' + user);
-      return res.json({code:200,result:user});
+      // 토큰 삭제 전 유효시간을 0으로 처리
+      const token = jwt.sign({userId:userId},process.env.JWT_SECRET,{
+        expiresIn: 0,
+        issuer:'wishlist'
+      });
+      
+      return res.json({code:200,result:token});
     })
-    
   }
-
 )
+
+// 캐시 재발급(로그인 상태 유지를 선택했을 시)
 
 
 
@@ -144,7 +170,27 @@ router.get('/register',verifyToken,(req,res)=>{
 
 
 // 회원탈퇴
+router.get('/deleteAccount',verifyToken,(req,res)=>{
+  const userId = req.decoded.userId;
 
+    User.findOne({
+      userId:userId
+    },(err,user)=>{
+      
+      if(err){
+        console.error('에러발생');
+        return res.status(500).json({code:500,message:'서버에러'});
+      }
+      // 회원 탈퇴전 토큰의 유효시간을 0으로 처리
+      const token = jwt.sign({userId:userId},process.env.JWT_SECRET,{
+        expiresIn: 0,
+        issuer:'wishlist'
+      });
+      
+      return res.json({code:200,result:token});
+    })
+  }
+)
 
 
 
