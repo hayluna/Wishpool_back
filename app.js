a8024e0b7c99650485d3b57a8850b76c76517145
HaeEun Myung <hayluna2@gmail.com>
2019-12-17 05:56:02 +0900

    websocket test

--- app.js
+++ app.js
@@ -16,15 +16,15 @@ var corsOptions = {
 
 app.use(cors(corsOptions));
 // 웹소켓 
-var server = require('http').createServer(app); //웹 소켓을 위한 새로운 서버를 만든다.
+// var server = require('http').createServer(app); //웹 소켓을 위한 새로운 서버를 만든다.
 
-//웹소켓 서버는 3001번에서 listening하고 있다.
-server.listen(3001);
+// //웹소켓 서버는 3001번에서 listening하고 있다.
+// server.listen(3001);
 
 //socket연결 및 on, event행동정보가 담긴 socket.js모듈을 불러온다.
 //socket.js모듈은 웹서버를 파라미터로 받는 함수이다.
 var io = require('socket.io')(server); // 웹소켓 함수 실행
-io.attach(server);
+io.attach(3001);
 
 s={};
 s.connectedClients={};
