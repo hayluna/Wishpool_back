8188ba02ac0c8ca97dcec4eed2d23ad4811ec746
HaeEun Myung <hayluna2@gmail.com>
2019-12-17 06:29:08 +0900

    tet

--- app.js
+++ app.js
@@ -4,32 +4,28 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var cors = require('cors') //cors설정
 require('dotenv').config();
 
 
-var app = express();
 
-
-var corsOptions = {
-  origin: '*',};
-
-app.use(cors(corsOptions));
 // 웹소켓 
-// var server = require('http').createServer(app); //웹 소켓을 위한 새로운 서버를 만든다.
+var server = require('http').createServer(app); //웹 소켓을 위한 새로운 서버를 만든다.
 
-// //웹소켓 서버는 3001번에서 listening하고 있다.
-// server.listen(3001);
+//웹소켓 서버는 3001번에서 listening하고 있다.
+server.listen(3001, function(){
+  console.log('3001번에서 소리질러!!!!');
+})
 
 //socket연결 및 on, event행동정보가 담긴 socket.js모듈을 불러온다.
 //socket.js모듈은 웹서버를 파라미터로 받는 함수이다.
-var io = require('socket.io')({path:'/socket.io'}); // 웹소켓 함수 실행
-io.attach(3001);
+var io = require('socket.io')(server); // 웹소켓 함수 실행
+io.attach(server);
 
 s={};
 s.connectedClients={};
-console.log("여기!!!!!!!!!!\n\n\n\n",io.origins(['http://52.231.107.71:3001']));
+
 io.on('connection', function(socket){
+  console.log("여기!!!!!!!!!!\n\n\n\n",socket.id);
   socket.emit('giveSid', socket.id);
   socket.on('receiveUid', uid=>{
     s.connectedClients[uid] = {id:socket.id};
@@ -55,13 +51,20 @@ var followRouter = require('./routes/follow');
 // 몽고디비 : ./schemas/index.js의 module.exports로 내보낸 함수 실행
 var connect = require('./schemas');
 
+var cors = require('cors') //cors설정
+
+var app = express();
 
 // 몽고디비: express객체 생긴 후 몽고디비 연결
 connect();
 
 //웹소켓통신을 위해서는 쌍방 cors설정이 되어야한다.
 //클라이언트의 주소를 허용한다.
-// app.use(cors()) //cors use
+// var corsOptions = {
+//   origin: '*',};
+
+// app.use(cors(corsOptions));
+app.use(cors()) //cors use
 
 
 // view engine setup
