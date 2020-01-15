2715da7ab14591cbd1cb4debf51a8e4e84bf342d
HaeEun Myung <hayluna2@gmail.com>
2020-01-15 20:10:29 +0900

    ssl

--- app.js
+++ app.js
@@ -29,8 +29,8 @@ connect();
 //클라이언트의 주소를 허용한다. 서버의 주소를 허용하는 헤더를 붙여 보낸다.
 app.use(cors()) //cors use
 
-var targetPath = '.well-known/acme-challenge/O7TM4KLfRuiNcP6Pfnshlnk8eWYZOoR1UznSVgQf704';
-var returnValue = 'O7TM4KLfRuiNcP6Pfnshlnk8eWYZOoR1UznSVgQf704.YK-nj0Rj5X_HLRTP1xL5domWVq8iAw-TwAXZ6TtFqAQ';
+var targetPath = '.well-known/acme-challenge/3nlcguB--AUQwQiQHGSDRimv40fJZmNwZCqcLT24n8w';
+var returnValue = '3nlcguB--AUQwQiQHGSDRimv40fJZmNwZCqcLT24n8w.YK-nj0Rj5X_HLRTP1xL5domWVq8iAw-TwAXZ6TtFqAQ';
 
 app.get(targetPath, function(req, res){
   res.writeHead(200, {'Content-Type': 'text/plain'});
