c35bbcacb11d504e7a424b37fb6ba1225c020446
HaeEun Myung <hayluna2@gmail.com>
2020-01-15 19:40:43 +0900

    public static

--- app.js
+++ app.js
@@ -69,9 +69,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', express.static(path.join(__dirname, 'public')));
+
 app.use('/category', categoryRouter);
 app.use('/group', groupRouter);
-app.use('/', indexRouter);
 app.use('/item', itemRouter);
 app.use('/users', usersRouter);
 app.use('/test', dummyRouter);
